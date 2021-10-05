68b013acd0b88e1394e29f2695d4115f75d203eb
glebshalimov <glebshalimovru@gmail.com>
2021-10-05 22:59:12 +0300

    fix

--- AppleMusicApi/AppleMusic.cs
+++ AppleMusicApi/AppleMusic.cs
@@ -29,29 +29,6 @@ namespace AppleMusicApi
 
         public void Auth(string token)
         {
-            //var iat = Math.Round((DateTime.UtcNow.AddMinutes(-1) - new DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds, 0);
-            //var exp = Math.Round((DateTime.UtcNow.AddDays(180) - new DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds, 0);
-
-            //var payload = new Dictionary<string, object>()
-            //{
-            //    { "iss", "DA4VB3TQAS" },
-            //    { "iat", iat },
-            //    { "exp", exp }
-            //};
-
-            //var extraHeader = new Dictionary<string, object>()
-            //{
-            //    { "alg", "ES256" },
-            //    //{ "typ", "JWT" },
-            //    { "kid", "PX855CNRP6" }
-            //};
-
-            //var keyString = "MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgPJ0PmfjIpW5subs9imAx7JJbfs9tFnaEwwWAPX2AlUigCgYIKoZIzj0DAQehRANCAAT39UbxzAT9wN6RYUCKeldWGILB1p1pGsnJk7Lwb2NeQvTAmj14Y5naNe+AZMBRHVHsCkIu/c5F4WOjfwiACNc2";
-
-            //CngKey privateKey = CngKey.Import(Convert.FromBase64String(keyString), CngKeyBlobFormat.Pkcs8PrivateBlob);
-
-            //string token1 = JWT.Encode(payload, privateKey, JwsAlgorithm.ES256, extraHeader);
-
             AccessToken = token;
         }
 
