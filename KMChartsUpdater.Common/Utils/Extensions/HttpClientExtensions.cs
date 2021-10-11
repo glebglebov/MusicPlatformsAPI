f733ba6ec19dee329601973b8af9387803af8c97
glebshalimov <glebshalimovru@gmail.com>
2021-10-11 23:18:45 +0300

    common

---
+++ KMChartsUpdater.Common/Utils/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http;
+
+namespace KMChartsUpdater.Common.Utils.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static void AddHeader(this HttpClient client, string name, string value)
+        {
+            client.DefaultRequestHeaders.TryAddWithoutValidation(name, value);
+        }
+    }
+}
